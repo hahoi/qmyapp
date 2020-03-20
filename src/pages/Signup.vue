efeec94a970a5d15185d42fc284301c3c48c420e
haho <haho@hotmail.com>
2020-03-20 16:08:18 +0800

    signup

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -7,10 +7,29 @@
 
       <q-card-section>
         <div class="q-gutter-md">
-          <form>
-            <q-input v-model="user.email" label="E-mail" :rules="[rules.required, rules.email]" />
-            <q-input v-model="user.id" label="帳號" :rules="[rules.required]" />
-            <q-input v-model="user.password" :type="isPwd ? 'password' : 'text'" label="密碼">
+          <form
+            ref="form"
+            @submit.prevent.stop="onSubmit"
+            @reset.prevent.stop="onReset"
+            class="q-gutter-md"
+          >
+            <q-input  ref="email" v-model="user.email" label="E-mail" :rules="[rules.required, rules.email]" />
+            <q-input v-model="user.name" label="姓名" :rules="[rules.required]" />
+            <q-input v-model="user.alias" label="登入代號" :rules="[rules.required]" />
+            <q-select
+              v-model="user.department"
+              :options="department"
+              label="單位名稱"
+              :rules="[rules.required]"
+            />
+            <q-input v-model="user.telephone" label="連絡電話" :rules="[rules.required]" />
+
+            <q-input
+              v-model="password"
+              :type="isPwd ? 'password' : 'text'"
+              label="密碼"
+              :rules="[rules.min]"
+            >
               <template v-slot:append>
                 <q-icon
                   :name="isPwd ? 'visibility_off' : 'visibility'"
@@ -19,13 +38,37 @@
                 />
               </template>
             </q-input>
+
+            <q-input
+              v-model="user.password"
+              :type="isPwd ? 'password' : 'text'"
+              label="再輸入相同的密碼"
+              :rules="[rules.min, rules.passwordMath]"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="isPwd = !isPwd"
+                />
+              </template>
+            </q-input>
+            <q-btn label="註冊" type="submit" color="primary" />
+            <q-btn
+              label="返回"
+              type="reset"
+              color="primary"
+              flat
+              class="q-ml-sm"
+              @click="$router.go(-1)"
+            />
           </form>
         </div>
       </q-card-section>
 
       <q-separator dark />
 
-      <q-card-actions align="right">
+      <!-- <q-card-actions align="right">
         <q-btn label="註冊" type="submit" color="primary" />
         <q-btn
           label="返回"
@@ -35,15 +78,18 @@
           class="q-ml-sm"
           @click="$router.go(-1)"
         />
-      </q-card-actions>
+      </q-card-actions>-->
     </q-card>
   </q-page>
 </template>
 
 <script>
+import { dbFirestore, dbAuth, dbFunctions } from "../fb";
 export default {
   data() {
     return {
+      department: [],
+      password: "",
       user: {
         //使用者資料
         authId: "",
@@ -59,7 +105,7 @@ export default {
       isPwd: true,
       rules: {
         required: value => !!value || "這個欄位必須要輸入",
-        min: v => v.length >= 8 || "最少需要8個字元",
+        min: v => v.length >= 6 || "最少需要6個字元",
         email: value => {
           const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
           return pattern.test(value) || "不合格式的 e-mail.";
@@ -73,6 +119,28 @@ export default {
         // emailMatch: () => "The email and password you entered don't match"
       }
     };
+  },
+  mounted() {
+    dbFirestore
+      .collection("SettingData")
+      .doc("Department") //單位
+      .get()
+      .then(doc => {
+        let temp = doc.data().depart;
+        temp
+          .sort(function(a, b) {
+            return a.order - b.order; //小的排在前面，注意字串排序，用減號 不是 <
+          })
+          .forEach(item => {
+            this.department.push(item.title);
+          });
+      });
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.$refs.email.validate())
+    },
+    onReset() {}
   }
 };
 </script>
